c816ad117fa45345648f51187dadea4fb652b758
ThatLuaDoggo <149950718+ThatLuaDoggo@users.noreply.github.com>
2023-11-08 19:09:22 +0100

    Update and rename drawingAPI.lua to colors.lua

---
+++ Minecraft/colors.lua
@@ -0,0 +1,35 @@
+local Colors = {
+  Light = {
+    Red = "0xe8504a",
+    Orange = "0xf78b48",
+    Yellow = "0xf7cd59",
+    Green = "0x8ae35d",
+    Blue = "0x6494ed",
+    Pink = "0xf18cf5"
+  },
+  Dark = {
+    Red = "0xba2c27",
+    Orange = "0xf56a14",
+    Yellow = "0xf5b402",
+    Green = "0x48c20c",
+    Blue = "0x246bf0",
+    Pink = "0xf44dfa"
+  },
+  __Information = {
+    Light = {
+      White = "0xFFFFFF",
+      Black = "0x000000",
+      Dark = "0x2e2e30",
+      Request = "0xed9b58",
+      Input = "0xf5af76"
+    },
+    Dark = {
+      White = "0xcfd0e6",
+      Black = "0x000000",
+      Dark = "0x2e2e30",
+      Request = "0xed9b58",
+      Input = "0xf5af76"
+    }
+  }
+}
+return Colors
